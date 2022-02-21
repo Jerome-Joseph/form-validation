29610858a518ab70cbd03af0a0911e64bc4005fc
Jerome-Joseph <jeromejoseph782@gmail.com>
2022-02-21 21:28:43 +0530

    commit Final

--- src/App.js
+++ src/App.js
@@ -1,23 +1,32 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container">
+      <h2 className='text-center mt-3'>Form Validation</h2>
+      <form>
+        <div className="mb-3">
+          <label htmlFor="exampleInputEmail1" className="form-label">Name</label>
+          <input type="text" className="form-control" id="name" minLength={5} required aria-describedby="emailHelp"/>
+        </div>
+        <div className="mb-3">
+          <label htmlFor="exampleInputEmail1" className="form-label">Email address</label>
+          <input type="email" className="form-control" id="email" required aria-describedby="emailHelp"/>
+        </div>
+        <div className="mb-3">
+          <label htmlFor="exampleInputPassword1" className="form-label">Password</label>
+          <input type="password" className="form-control" id="password" minLength={5} required/>
+        </div>
+        <div className="mb-3">
+          <label htmlFor="exampleInputPassword1" className="form-label">City</label>
+          <input type="text" className="form-control" id="city" minLength={4} required/>
+        </div>
+        <div className="mb-3">
+          <label htmlFor="exampleInputPassword1" className="form-label">Phone Number</label>
+          <input type="text" pattern="\d*" className="form-control" id="number" minLength={10} maxLength={10} required/>
+        </div>
+        <button type="submit" className="btn btn-primary">Submit</button>
+      </form>
     </div>
   );
 }
